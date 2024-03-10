5d7fccac1597df4422867dbb064aef3d57874f8a
pragap <68117603+pragap@users.noreply.github.com>
2024-03-11 02:50:50 +0530

    Update redingcsv.py

--- redingcsv.py
+++ redingcsv.py
@@ -13,49 +13,9 @@ from pyspark.sql.functions import col,array_contains
 
 spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
 
-df = spark.read.csv("/FileStore/zipcodes.csv")
-
-df.printSchema()
-
 df2 = spark.read.option("header",True) \
-     .csv("/FileStore/zipcodes.csv")
-df2.printSchema()
-   
-
-
-df3 = spark.read.options(header='True', delimiter=',') \
-  .csv("/FileStore/zipcodes.csv")
-df3.printSchema()
-
-
-schema = StructType() \
-      .add("RecordNumber",IntegerType(),True) \
-      .add("Zipcode",IntegerType(),True) \
-      .add("ZipCodeType",StringType(),True) \
-      .add("City",StringType(),True) \
-      .add("State",StringType(),True) \
-      .add("LocationType",StringType(),True) \
-      .add("Lat",DoubleType(),True) \
-      .add("Long",DoubleType(),True) \
-      .add("Xaxis",IntegerType(),True) \
-      .add("Yaxis",DoubleType(),True) \
-      .add("Zaxis",DoubleType(),True) \
-      .add("WorldRegion",StringType(),True) \
-      .add("Country",StringType(),True) \
-      .add("LocationText",StringType(),True) \
-      .add("Location",StringType(),True) \
-      .add("Decommisioned",BooleanType(),True) \
-      .add("TaxReturnsFiled",StringType(),True) \
-      .add("EstimatedPopulation",IntegerType(),True) \
-      .add("TotalWages",IntegerType(),True) \
-      .add("Notes",StringType(),True)
-      
-#df_with_schema = spark.read.format("csv") \
-#      .option("header", True) \
-#      .schema(schema) \
-#      .load("/FileStore/zipcodes.csv")
-#df_with_schema.printSchema()
+     .csv("dbfs:/FileStore/zipcodes.csv")
 
 df2.write.option("header",True) \
- .csv("/FileStore/zipcodes1")
+ .csv("dbfs/FileStore/zipcodes1")
  
